9ac02b8b617939db15257aee6eaf51fbb32417e6
mehmeteyupoglu2 <mehmeteyupoglu2@gmail.com>
2020-12-26 17:49:51 +0300

    Add testing-library-dom for cleaner test writin

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,14 @@
 import ReactDOM from "react-dom";
 import App from "./App";
+import { getQueriesForElement } from "@testing-library/dom";
 
 test("renders learn the correct content", () => {
   const root = document.createElement("div");
   ReactDOM.render(<App />, root);
 
-  expect(root.querySelector("h1").textContent).toBe("TODOS");
-  expect(root.querySelector("label").textContent).toBe(
-    "What needs to be done?"
-  );
-  expect(root.querySelector("button").textContent).toBe("Add #1");
+  const { getByText, getByLabelText } = getQueriesForElement(root);
+
+  expect(getByText("TODOS")).not.toBeNull();
+  expect(getByLabelText("What needs to be done?")).not.toBeNull();
+  expect(getByText("Add #1")).not.toBeNull();
 });
